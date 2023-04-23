7ba2b2ce7948c448bd1b0fb1ba29482910fd4350
Mario Rojas <16016384+TURROKS@users.noreply.github.com>
2023-04-23 16:18:35 +0400

    Improved error output format

--- transforms/WebsiteToScreenshotCacheXL.py
+++ transforms/WebsiteToScreenshotCacheXL.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import requests
 
 from extensions import registry, web2screenshot_set
-from maltego_trx.maltego import MaltegoMsg, MaltegoTransform
+from maltego_trx.maltego import MaltegoMsg, MaltegoTransform, UIM_FATAL, UIM_PARTIAL
 from maltego_trx.transform import DiscoverableTransform
 from maltego_trx.overlays import OverlayType, OverlayPosition
 
@@ -18,8 +18,7 @@ def api_handler(api_url, target_url, api, custom_agent):
 
     data = requests.get(api_url+f"?access_key={api}&url=https://{target_url}&full_page=true&user_agent={custom_agent}")
 
-    if data.status_code == 200:
-        return data.url
+    return data
 
 
 def api_info(api):
@@ -50,15 +49,30 @@ class WebsiteToScreenshotCacheXL(DiscoverableTransform):
         clean_url = parse.quote_plus(input_url)
 
         screenshot = api_handler(api_url=api_baseurl, target_url=clean_url, api=api_key, custom_agent=user_agent)
-        api_check = api_info(api=api_key)
 
-        if screenshot:
-            ent = response.addEntity("maltego.Image", str(input_url).lower())
-            ent.addProperty("url", "URL", "strict", screenshot)
-            ent.addProperty(fieldName="captureType", displayName="Capture Type", matchingRule='strict', value="Cache")
-            ent.addProperty(fieldName="captureSize", displayName="Capture Size", matchingRule='strict', value="Standard")
-            ent.addOverlay("#fcba03", OverlayPosition.NORTH_WEST, OverlayType.COLOUR)
-            ent.addDisplayInformation(content=f'<a href="{screenshot}">Open in Browser</a>', title="Screenshot")
-            response.addUIMessage(f"API Flash Transform runs: {api_check.get('remaining')} of {api_check.get('limit')} "
-                                  f"credits remaining. Current quota period ends at "
-                                  f"{dt.fromtimestamp(api_check.get('reset'))}")
+        if screenshot.status_code == 200:
+            api_check = api_info(api=api_key)
+
+            if screenshot:
+                ent = response.addEntity("maltego.Image", str(input_url).lower())
+                ent.addProperty("url", "URL", "strict", screenshot.url)
+                ent.addProperty(fieldName="captureType", displayName="Capture Type", matchingRule='strict', value="Cache")
+                ent.addProperty(fieldName="captureSize", displayName="Capture Size", matchingRule='strict', value="Standard")
+                ent.addOverlay("#fcba03", OverlayPosition.NORTH_WEST, OverlayType.COLOUR)
+                ent.addDisplayInformation(content=f'<a href="{screenshot.url}">Open in Browser</a>', title="Screenshot")
+                response.addUIMessage(f"API Flash Transform runs: {api_check.get('remaining')} of {api_check.get('limit')} "
+                                      f"credits remaining. Current quota period ends at "
+                                      f"{dt.fromtimestamp(api_check.get('reset'))}")
+        elif screenshot.status_code == 401:
+            response.addUIMessage("The access key used to make this API call has been revoked or is invalid!",
+                                  messageType=UIM_FATAL)
+        elif screenshot.status_code == 402:
+            response.addUIMessage("Payment Required", messageType=UIM_PARTIAL)
+        elif screenshot.status_code == 403:
+            response.addUIMessage("TForbidden!", messageType=UIM_PARTIAL)
+        elif screenshot.status_code == 429:
+            response.addUIMessage("Too Many Requests!", messageType=UIM_PARTIAL)
+        elif screenshot.status_code == 500:
+            response.addUIMessage("Internal Server Error!", messageType=UIM_PARTIAL)
+        else:
+            response.addUIMessage("Unknown Error", messageType=UIM_PARTIAL)
